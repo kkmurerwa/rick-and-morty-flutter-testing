66ca4b8712f254f958ef4a18e09f8c9bce42fe27
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-23 13:51:56 +0300

    feat: added more tests to get_characters_usecase_test for better test coverage

--- test/features/characters/domain/get_characters_usecase_test.dart
+++ test/features/characters/domain/get_characters_usecase_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:ramft/features/characters/domain/entities/character.dart';
+import 'package:ramft/core/errors/failures.dart';
 import 'package:ramft/features/characters/domain/repositories/characters_repository.dart';
 import 'package:ramft/features/characters/domain/usecases/get_characters_usecase.dart';
 
@@ -18,14 +18,30 @@ void main() {
     getCharactersUseCase = GetCharactersUseCase(mockCharactersRepository);
   });
 
-  test('should get characters from the repository', () async {
+  test('should query repository to get characters on call invoked', () async {
+    when(() => mockCharactersRepository.getCharacters(any()))
+        .thenAnswer((_) async => const Right([tCharacter]));
+
+    await getCharactersUseCase.call(const Params(page: tPage));
+
+    verify(() => mockCharactersRepository.getCharacters(tPage));
+  });
+
+  test('should get characters from the repository if success', () async {
     when(() => mockCharactersRepository.getCharacters(any()))
       .thenAnswer((_) async => const Right([tCharacter]));
 
     final result = await getCharactersUseCase.call(const Params(page: tPage));
 
     expect(result, const Right([tCharacter]));
+  });
 
-    verify(() => mockCharactersRepository.getCharacters(tPage));
+  test('should return failure from repository if failure', () async {
+    when(() => mockCharactersRepository.getCharacters(any()))
+        .thenAnswer((_) async => const Left(ServerFailure("")));
+
+    final result = await getCharactersUseCase.call(const Params(page: tPage));
+
+    expect(result, const Left(ServerFailure("")));
   });
 }
