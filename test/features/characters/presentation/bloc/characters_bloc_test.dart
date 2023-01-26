6814ffeda5d7b3cdc0530604992e10aef8fdaf39
Kenneth Murerwa <xwaxes@gmail.com>
2023-01-26 17:24:54 +0300

    feat: Add sample bloc test

--- test/features/characters/presentation/bloc/characters_bloc_test.dart
+++ test/features/characters/presentation/bloc/characters_bloc_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -10,6 +11,7 @@ import '../../../../fixtures/test_models.dart';
 class MockGetCharactersUseCase extends Mock implements GetCharactersUseCase {}
 class MockParams extends Mock implements Params {}
 class MockFakeCharactersEvent extends Mock implements CharactersEvent {}
+class MockCharactersBloc extends MockBloc<CharactersEvent, CharactersState> {}
 
 void main() {
   late MockGetCharactersUseCase mockGetCharactersUseCase;
@@ -81,4 +83,22 @@ void main() {
     // act
     charactersBloc.add(MockFakeCharactersEvent());
   });
+
+  blocTest<CharactersBloc, CharactersState>('bloc test',
+      setUp: () async {
+        when(() => mockGetCharactersUseCase.call(const Params(page: 1)))
+            .thenAnswer((_) async => const Right([tCharacter]));
+      },
+      build: () => CharactersBloc(charactersUseCase: mockGetCharactersUseCase),
+      act: (bloc) {
+        bloc.add(const GetCharactersEvent(pageNumber: 1));
+      },
+      expect: () => <CharactersState>[
+        LoadingState(),
+        const SuccessState(characters: [tCharacter]),
+      ],
+      verify: (_) async {
+        verify((() => mockGetCharactersUseCase.call(const Params(page: 1)))).called(1);
+      }
+  );
 }
